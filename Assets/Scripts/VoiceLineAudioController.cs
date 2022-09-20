9b15abc4951ef463a11628a4ecef67612b95aab1
i-Jiro <nguyen.andy.dev@gmail.com>
2022-09-20 02:55:16 -0400

    Added voice lines on tracked.
    App will play Jotaro's voice lines when he is tracked on screen.

---
+++ Assets/Scripts/VoiceLineAudioController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class VoiceLineAudioController : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    [SerializeField] private List<AudioClip> _voiceLineClips;
+
+    void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    //Called by Unity event in target tracker's event handler.
+    public void PlayRandomVoiceLine()
+    {
+        if (_voiceLineClips.Count < 1)
+        {
+            Debug.LogError("No audio clips in voice line list!");
+        }
+        int index = Random.Range(0, _voiceLineClips.Count-1);
+        if (_voiceLineClips[index] != null)
+        {
+            _audioSource.PlayOneShot(_voiceLineClips[index]);
+        }
+    }
+}
